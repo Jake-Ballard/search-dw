c118dcdfe564ad099d25361a4869d0adce6e4408
Massimo Cuozzo <jake.ballard@usa.com>
2019-12-17 15:39:01 +0100

    Added progress bar and minor bugs

---
+++ README.md
@@ -0,0 +1,41 @@
+# What is
+**search-dw** is a Python utility to automate "search and download" via google search.
+	
+## Getting Started
+To run this script, you need to have **Python3 installed** on your system. 
+
+### Prerequisite
+The following pip are needed.
+```
+$ pip3 install requests
+$ pip3 install google
+$ pip3 install urllib3
+
+```
+### Download
+Download project from GitHub.
+```
+git clone https://github.com/Jake-Ballard/search-dw.git && cd search-dw
+```
+
+### Examples
+Here you are some examples that shows you how to search and download somenthing based on one or more keywords.
+```
+$ python3 search-dw.py -s "Ruby doc" -e pdf
+
+$ python3 search-dw.py -s "Security Workshop" -e ppt -p 10 -r 5
+
+$ python3 search-dw.py -s "Project Management Test Exercise" -e pdf -p 20 -r 40
+
+```
+
+
+## Feedback
+
+Your suggestions are kindly [welcome](https://github.com/Jake-Ballard/search-dw/issues)!
+
+## Warning
+
+Use the **search-dw.py** script at your own risk.  
+
+
--- search-dw.py
+++ search-dw.py
@@ -3,8 +3,9 @@ import argparse
 import requests
 from googlesearch import search
 from urllib.parse import unquote
+from urllib.error import HTTPError
 from time import time
-
+from clint.textui import progress
 # Custom message from argparse
 
 
@@ -14,10 +15,9 @@ def msg(name=None):
           -e or --ext,                Filetype of searched file
           -p or --page   [optional],  Number of google page to consider
           -r or --result [optional],  Result limit
-
          Example:
             1. python3 search-dw.py -s "Ruby doc" -e pdf
-            2. puthon3 search-dw.py -s forking -e ppt -p 10 -r 5
+            2. python3 search-dw.py -s "Security Workshop" -e ppt -p 10 -r 5
          '''
 
 # Set file name based on url retrived
@@ -60,31 +60,39 @@ except:
 # Move in download dir
 os.chdir(dirName)
 
-# Set counter & monitor time exec
+# Set counter & monitor time exec & prepare header
 i = 0
 start = time()
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 print()
-print("Downloading file...")
+print("Downloading of << " + args.search + " >> is started. Be patient :-)")
 print()
 
-for j in search(query, tld="com", num=args.page, stop=args.result, pause=10.0):
-    # Ignore bad request and move on
-    try:
-        r = requests.get(j, timeout=5)
-
-        # Get file size of file retrieved
-        size = int(r.headers['Content-length'])/1000000
-
-        # Download only file > 0.5 MB
-        if (size > 0.5):
-            i += 1
-            fileN = setFileName(j)
-            print(i, fileN, str(round(size, 1)) + " MB")
-            with open(fileN, "wb") as f:
-                f.write(r.content)
-    except:
-        pass
-
+try:
+    for j in search(query, tld="com", num=args.page, stop=args.result, pause=10.0):
+        # Ignore bad request and move on
+        try:
+            r = requests.get(j, timeout=5, headers=headers)
+            # Get file size of file retrieved
+            size = int(r.headers['Content-length'])
+            # Download only file > 100 KB
+            if (size > 100000):
+                i += 1
+                fileN = setFileName(j)
+                with open(fileN, "wb") as f:
+                    for bar in progress.bar(r.iter_content(chunk_size=1024), expected_size=(size/1024) + 1, label=fileN + " - "):
+                        if bar:
+                            f.write(bar)
+                            f.flush()
+        except:
+            pass
+except HTTPError as e:
+    print("Houston we've got a problem!!!")
+    print(e.headers)
 print()
-print(f"Time to download: {time() - start}")
+
+f_dw = "file" if i == 1 else "files"
+
+print(f"Downloaded {i} {f_dw} in {time() - start} seconds")
